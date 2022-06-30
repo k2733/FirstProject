eafbfdbe36355007c631b8625ff8d8993d77fdfc
不好笑的呀 <18296651292@163.com>
2022-06-30 17:18:47 +0800

    20220630.2

---
+++ .history/app/O_Data_20220630165711.py
@@ -0,0 +1,36 @@
+from typing_extensions import Self
+import cx_Oracle
+# coding:utf-8
+
+# file => dict
+
+file_dict = {}
+
+
+#增（insert）
+
+#删（delete）
+
+#查（select）
+
+#改（update）
+
+class ConnO(Self):
+    tns = cx_Oracle.makedsn("dx.huangyi.cn","1521","orcl")  #监听Oracle数据库
+    # 建立和数据库系统的连接
+    conn = cx_Oracle.connect("C##VCC","VCC",tns)   #连接数据库
+
+
+    def read_data():
+        cursor = ConnO.conn.cursor()
+        cursor.execute("select * from tb_user")
+        #读取整个表格
+        one = cursor.fetchall()
+        return one
+
+    
+        
+
+
+if __name__ == "__main__":
+    ConnO.read_data()
---
+++ .history/app/O_Data_20220630170216.py
@@ -0,0 +1,39 @@
+from unicodedata import name
+import cx_Oracle
+import pandas as pd
+# coding:utf-8
+
+# file => dict
+
+file_dict = {}
+
+
+#增（insert）
+
+#删（delete）
+
+#查（select）
+
+#改（update）
+
+class ConnO():
+    tns = cx_Oracle.makedsn("dx.huangyi.cn","1521","orcl")  #监听Oracle数据库
+    # 建立和数据库系统的连接
+    conn = cx_Oracle.connect("C##VCC","VCC",tns)   #连接数据库
+
+
+    def read_data():
+        cursor = ConnO.conn.cursor()
+        cursor.execute("select * from tb_user")
+        #读取整个表格
+        one = cursor.fetchall()
+        file1 = pd.DataFrame(one,name=['ID','user','password'])
+
+        return print(file1)
+
+    
+        
+
+
+if __name__ == "__main__":
+    ConnO.read_data()
